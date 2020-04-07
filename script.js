2defcedcf988f72147c723ef46484e6adf0718fa
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-04-06 20:58:15 -0600

    added local stoarage get and set

--- script.js
+++ script.js
@@ -1,50 +1,84 @@
 var now = moment().format("LLL");
 var APIKey = "9ceffc16572e37c6256c7430926365a7";
 
-$('#searchBtn').click(function(){
-     
+$('#historyBtn').click(function () {
+  event.preventDefault()
+  alert('here')
+  console.log(this)
+})
+$('#searchBtn').click(function () {
+  event.preventDefault()
   var findCity = $('#citySearch').val().trim();
 
+  getCurrent(findCity)
+})
+function getCurrent(findCity) {
   // Here we are building the URL we need to query the database
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+findCity+",us&units=imperial&appid=" + APIKey;
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + findCity + ",us&units=imperial&appid=" + APIKey;
   // We then created an AJAX call
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-     imageString = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
-     city = (`<p><h3>${findCity} ${now} <img src="${imageString}" alt="weatherIcon"></p>`)
-     temp = (`<p>Temperature: ${response.main.temp}</p>`)
-     humidity = (`<p>Humidity: ${response.main.humidity}</p>`)
-     wind = (`<p>Wind: ${response.wind.speed}</p>`)
-     $("#currentForecast").append(city,temp,humidity,wind)
+  }).then(function (response) {
+    $('#currentForecast').empty()
+    imageString = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+    city = (`<p><h3>${findCity} ${now} <img src="${imageString}" alt="weatherIcon"></p>`)
+    temp = (`<p>Temperature: ${response.main.temp}</p>`)
+    humidity = (`<p>Humidity: ${response.main.humidity}</p>`)
+    wind = (`<p>Wind: ${response.wind.speed}</p>`)
+    $("#currentForecast").append(city, temp, humidity, wind)
+
+    historyList(findCity)
     futureForecast(findCity)
 
   });
 
-})
-
-function futureForecast(findCity)
-{
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+findCity+",us&units=imperial&appid=" + APIKey;
+}
+function futureForecast(findCity) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + findCity + ",us&units=imperial&appid=" + APIKey;
 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(results) {
-   
-    for (var i = 0; i< 5;i++){
-      day = `<p>${results.list[i].dt_txt}</p>`
-      icon =  `http://openweathermap.org/img/wn/${results.list[i].weather[0].icon}.png`
-      imageSting =  (`<p><img src="${icon}" alt="weatherIcon"></p>`)
-      temp = (`<p>Temperature: ${results.list[i].main.temp}</p>`)
-      humidity = (`<p>Humidity: ${results.list[i].main.humidity}</p>`)
-      addTo = '#day'+i
-      $(addTo).append(day,imageSting ,temp,humidity)
-    
+  }).then(function (results) {
+    $('#futureForecast').empty()
+    for (var i = 0; i < 14; i++) {
+      if (i === 1 || i === 4 || i == 7 || i == 10 || i == 13) {
+        newDiv = $('<div class="card">')
+        divBody = $('<div class="card-body">')
+
+        day = `<p>${results.list[i].dt_txt}</p>`
+        icon = `http://openweathermap.org/img/wn/${results.list[i].weather[0].icon}.png`
+        imageSting = (`<p><img src="${icon}" alt="weatherIcon"></p>`)
+        temp = (`<p>Temperature: ${results.list[i].main.temp}</p>`)
+        humidity = (`<p>Humidity: ${results.list[i].main.humidity}</p>`)
+        divBody.append(day, imageSting, temp, humidity)
+        newDiv.append(divBody)
+        $('#futureForecast').append(newDiv)
+
+      }
     }
-    
-    console.log(results)
+
 
   });
+}
+
+function historyList(city) {
+
+  localStorage.setItem(city, city);
+  populateHistoryList()
+
+}
+function populateHistoryList() {
+  $('#historyList').empty()
+  newList = $(' <div class="list-group" id="historyBtn">')
+  for (var i = 0; i < localStorage.length; i++) {
+    newList.append(`<button type="button" class="list-group-item list-group-item-action ">${localStorage.key(i)}</button>`)
+
+  }
+  $('#historyList').append(newList)
+}
+window.onload = function () {
+  this.getCurrent('denver')
+  this.populateHistoryList()
 }
